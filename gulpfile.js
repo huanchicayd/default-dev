4f3994345378721081b8f30723eac6803eca55d9
Daniel Huanchicay <daniel@danielhuanchicay.com>
2017-11-21 15:27:42 -0200

    Atualização dos diretórios e plugins do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,55 @@
 var gulp = require('gulp');
-imagemin = require('gulp-imagemin');
-imageminJpegRecompress = require('imagemin-jpeg-recompress');
 clean = require('gulp-clean');
-uglify = require('gulp-uglify');
 cssmin = require('gulp-cssmin');
 htmlmin = require('gulp-htmlmin');
+uglify = require('gulp-uglify');
+imagemin = require('gulp-imagemin');
+imageminJpegRecompress = require('imagemin-jpeg-recompress');
 
 gulp.task('default', ['copy'], function(){
-    gulp.start('build-img', 'build-js', 'build-css', 'build-html');
+  gulp.start('build-img', 'build-js', 'build-css', 'build-html');
 });
 
 gulp.task('copy', ['clean'], function(){
-    return gulp.src([
-        'dev/**/*',
-        '!dev/styles/**'
-    ])
-        .pipe(gulp.dest('dist'));
+  return gulp.src([
+      'dev/**/*',
+      '!dev/styles/**'
+  ])
+      .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function(){
-    return gulp.src('dist')
-        .pipe(clean());
+  return gulp.src('dist')
+      .pipe(clean());
 });
 
 gulp.task('build-img', function(){
-    gulp.src('dev/images/**/*')
-        .pipe(imagemin([
-            imageminJpegRecompress({
-                loops: 6,
-                min: 40,
-                max: 85,
-                quality: 'low'
-            })
-        ]))
-        .pipe(gulp.dest('dist/images'));
+  gulp.src('dev/images/**/*')
+      .pipe(imagemin([
+          imageminJpegRecompress({
+              loops: 6,
+              min: 40,
+              max: 85,
+              quality: 'low'
+          })
+      ]))
+      .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('build-js', function(){
-    gulp.src('dev/scripts/**/*')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/scripts'));
+  gulp.src('dev/scripts/**/*')
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/scripts'));
 });
 
 gulp.task('build-css', function(){
-    gulp.src('dev/styles/*.css')
-        .pipe(cssmin())
-        .pipe(gulp.dest('dist/styles'));
+  gulp.src('dev/styles/*.css')
+      .pipe(cssmin())
+      .pipe(gulp.dest('dist/styles'));
 });
 
 gulp.task('build-html', function(){
-    gulp.src('dev/*.php')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist'));
+  gulp.src('dev/*.php')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('dist'));
 });
